55d51b33c30b5c19237d3d7192f0d4ddd76051e7
martsDev <martssilvapro@gmail.com>
2022-07-20 14:51:15 -0300

    add Home in app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div className="App">
-     Hello mundo!
+      <Home />
     </div>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Popular from '../components/Popular';
+import Veggie from '../components/Veggie';
+
+const Home = () => {
+  return (
+    <section>
+        <Popular />
+        <Veggie />
+    </section>
+  );
+};
+
+export default Home;
