59476000f6a6b53201184063c15b188b165efb76
martsDev <martssilvapro@gmail.com>
2022-07-21 12:39:56 -0300

    add style on navbar

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -8,22 +8,22 @@ const Category = () => {
   return (
     <section>
         <List>
-            <NavLink to={'/cuisine/italian'}>
+            <StyledLink to={'/cuisine/italian'}>
                 <FaPizzaSlice />
                 <h4>Itália</h4>
-            </NavLink>
-            <NavLink to={'/cuisine/american'}>
+            </StyledLink>
+            <StyledLink to={'/cuisine/american'}>
                 <FaHamburger />
                 <h4>América</h4>
-            </NavLink>
-            <NavLink to={'/cuisine/chinese'}>
+            </StyledLink>
+            <StyledLink to={'/cuisine/chinese'}>
                 <GiNoodles />
                 <h4>China</h4>
-            </NavLink>
-            <NavLink to={'/cuisine/japanese'}>
+            </StyledLink>
+            <StyledLink to={'/cuisine/japanese'}>
                 <GiChopsticks />
                 <h4>Japão</h4>
-            </NavLink>
+            </StyledLink>
         </List>
     </section>
   );
@@ -33,6 +33,35 @@ const List = styled.div`
     display: flex;
     justify-content: center;
     margin: 2rem 0rem;
+`;
+
+const StyledLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    width: 6rem;
+    height: 6rem;
+    cursor: pointer;
+    transform: scale(0.8);
+
+    h4 {
+        color: white;
+        font-size: 0.8rem;
+    }
+
+    svg {
+        color: white;
+        font-size: 1.5rem;
+    }
+
+    &.active {
+        background: linear-gradient(to right, #f27121, #e94057);
+    }
 `;
 
 export default Category;
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -52,6 +52,6 @@ const Card = styled.div`
         text-align: center;
         padding: 1rem;
     }
-`
+`;
 
 export default Cuisine;
