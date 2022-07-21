ac8f1b838aad5e05d318b70baed51042ef877bac
martsDev <martssilvapro@gmail.com>
2022-07-21 11:29:23 -0300

    fetch cuisine recipe based on params

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import {  motion  } from 'framer-motion';
 import {  Link, useParams  } from 'react-router-dom';
+import { getCuisine } from '../services/cuisine';
 
 const Cuisine = () => {
+    const [cuisine, setCuisine] = useState([]);
+    let params = useParams();
+
+    useEffect(() => {
+        const getCuisineRecipes = async (name) => {
+            const data = await getCuisine(name);
+            
+            setCuisine(data.results);
+        };
+
+        getCuisineRecipes(params.type);
+    }, [params.type]);
+
+    console.log(cuisine)
   return (
     <div>Cuisine</div>
   );
