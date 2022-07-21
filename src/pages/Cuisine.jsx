920da809b8a86b9aed914285dcf90306585a6bc7
martsDev <martssilvapro@gmail.com>
2022-07-21 12:25:29 -0300

    add cuisine recipe grid

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -18,10 +18,40 @@ const Cuisine = () => {
         getCuisineRecipes(params.type);
     }, [params.type]);
 
-    console.log(cuisine)
   return (
-    <div>Cuisine</div>
+    <Grid>
+        {cuisine.map((recipe) => {
+            return (
+                <Card key={recipe.id}>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <h4>{recipe.title}</h4>
+                </Card>
+            );
+        })}
+    </Grid>
   );
 };
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 15rem 15rem 15rem 15rem;
+    grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+    img {
+        width: 100%;
+        border-radius: 2rem;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    h4 {
+        text-align: center;
+        padding: 1rem;
+    }
+`
 
 export default Cuisine;
