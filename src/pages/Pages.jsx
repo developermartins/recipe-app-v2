1dcafa337cc68917d41b08f7d41d049572d5d51c
martsDev <martssilvapro@gmail.com>
2022-07-21 10:31:15 -0300

    add style on nav

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Home from './Home';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Cuisine from './Cuisine';
 
 const Pages = () => {
 
   return (
-    <div>
-        <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/cuisine' element={ <Cuisine /> } />
+      </Routes>
+    </Router>
   );
 };
 
