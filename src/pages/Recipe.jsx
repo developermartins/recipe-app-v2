9b95789684742059fb0cecaf0c87a2c2a71b6cd1
martsDev <martssilvapro@gmail.com>
2022-07-22 13:43:42 -0300

    small adjusts

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -88,6 +88,7 @@ const Button = styled.button`
   padding: 1rem 2rem;
   color: #313131;
   background: white;
+  border-radius: 5px;
   border: 2px solid black;
   margin-right: 2rem;
   font-weight: 600;
