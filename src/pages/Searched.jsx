3f0ecefec1df72138df3bddfbc1a53eaa776dd4d
martsDev <martssilvapro@gmail.com>
2022-07-22 10:51:05 -0300

    implements fecth searched dara

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getSearched } from '../services/searchedRecipe';
 
 const Searched = () => {
+
+  const [searchedRecipes, setSearchedRecipes] = useState();
+
+  const params = useParams();
+
+  useEffect(() => {
+    const getSearchedRecipe = async (params) => {
+      const data = await getSearched(params);
+
+      setSearchedRecipes(data.result);
+    };
+
+    getSearchedRecipe(params.search);
+  }, [params.search]);
+
+
   return (
     <div>Searched</div>
   );
--- src/services/searchedRecipe.js
+++ src/services/searchedRecipe.js
@@ -1,5 +1,5 @@
 export const getSearched = async (name) => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`);
+    const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`);
 
     const data = await api.json();
     return data;
