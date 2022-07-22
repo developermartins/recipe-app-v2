aa945c1391f801b0777e92fc9c26fbae75edeb58
martsDev <martssilvapro@gmail.com>
2022-07-22 10:42:30 -0300

    add searched fetch api

---
+++ src/services/searchedRecipe.js
@@ -0,0 +1,6 @@
+export const getSearched = async (name) => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`);
+
+    const data = await api.json();
+    return data;
+};
