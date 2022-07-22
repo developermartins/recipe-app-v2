f2cddea4d315d236a9c2aeebb0e9ad9476a3a320
martsDev <martssilvapro@gmail.com>
2022-07-22 11:22:23 -0300

    Add basics imports

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Recipe = () => {
   return (
