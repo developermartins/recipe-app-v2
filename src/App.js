a816fbf72dd06e977f33542ba81f496aa4ccae40
martsDev <martssilvapro@gmail.com>
2022-07-22 10:14:52 -0300

    add search component on home

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Category from './components/Category';
 import Pages from './pages/Pages';
 import {  BrowserRouter as Router } from 'react-router-dom';
+import Search from './components/Search';
+
+
 const App = () => {
   return (
     <div className="App">
       <Router>
+        <Search />
         <Category />
         <Pages />
       </Router>
