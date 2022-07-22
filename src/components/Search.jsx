ecff1ae4a06f50c0d2211ccb104486739e90d385
martsDev <martssilvapro@gmail.com>
2022-07-22 10:13:00 -0300

    add search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import {  FaSearch  } from 'react-icons/fa';
+
+const Search = () => {
+
+    const [] = useState(null);
+
+  return (
+    <FormStyle>
+        <input type="text" />
+    </FormStyle>
+  );
+};
+
+const FormStyle = styled.form`
+    margin: 0rem 20rem;
+    position: relative;
+    width: 100%;
+    input {
+        border: none;
+        background: linear-gradient(35deg, #494949, #313131);
+        font-size: 1.5rem;
+        color: white;
+        padding: 1rem 3rem;
+        border: none;
+        border-radius: 1rem;
+        outline: none;
+    }
+
+    svg {
+        position: absolute;
+        top: 50%;
+        left: 0;
+        transform: translate(100%, -50%);
+        color: white;
+    }
+`;
+
+export default Search;
