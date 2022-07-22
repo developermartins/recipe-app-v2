dc61f676be0ec30633878c42edbc8b21afecdbef
martsDev <martssilvapro@gmail.com>
2022-07-22 10:28:11 -0300

    implements basic search input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,19 +4,35 @@ import {  FaSearch  } from 'react-icons/fa';
 
 const Search = () => {
 
-    const [] = useState(null);
+    const [input, setInput] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('opa')
+    }
 
   return (
-    <FormStyle>
-        <input type="text" />
+    <FormStyle onSubmit={handleSubmit}>
+        <div>
+            <FaSearch></FaSearch>
+            <input 
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+        </div>
     </FormStyle>
   );
 };
 
 const FormStyle = styled.form`
     margin: 0rem 20rem;
-    position: relative;
-    width: 100%;
+
+    div {
+        position: relative;
+        width: 100%;
+    }
+
     input {
         border: none;
         background: linear-gradient(35deg, #494949, #313131);
@@ -26,6 +42,7 @@ const FormStyle = styled.form`
         border: none;
         border-radius: 1rem;
         outline: none;
+        width: 100%;
     }
 
     svg {
