de8c9f0609ebb258716579ed83545980ca4a1cc1
martsDev <martssilvapro@gmail.com>
2022-07-22 16:10:25 -0300

    add animation grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,12 @@ import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <motion.section>
+    <motion.section
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
         <Popular />
         <Veggie />
     </motion.section>
